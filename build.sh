1e744cd0e6d9b4fd90ec9992724e035950f6db5b
deept <194887172+Deepti767@users.noreply.github.com>
2026-03-31 18:11:43 +0530

    added build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
+
+python3 manage.py migrate --noinput
+
+python3 manage.py collectstatic --noinput
